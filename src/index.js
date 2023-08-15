06fcfafba7fbb7af9b02838ebd4e669ec047e831
rohankeenoy <103291728+rohankeenoy@users.noreply.github.com>
2023-08-15 09:20:11 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Portrait from './component/Background';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Portrait />
+  </React.StrictMode>
+);
